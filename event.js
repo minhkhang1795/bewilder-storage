35b713b68951920910b1df93a6dae794c720fdda
Khang Vu <khangvu@microsoft.com>
2022-02-06 03:23:40 -0800

    Add initial js files

---
+++ event.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize the FirebaseUI Widget using Firebase.
+  const db = firebase.firestore();
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  const uiConfig = {
+    signInSuccessUrl: window.location,
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  };
+  ui.start('#firebaseui-auth-container', uiConfig);
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    //var user = firebase.auth().currentUser;
+    if (user) {
+      console.log('signed in!');
+      let userName = user.displayName;
+      query("#registration-text").innerHTML = "Hi" + (userName ? " " + userName : "") + "! To join the event, please register below.";
+      query("#registration-form").style.display = "inline-block";
+      query("#login-button").style.display = "none";
+
+      db.collection('events').doc(eventId).get().then((doc) => {
+        if (!doc.exists)
+          return;
+
+        event = doc;
+        console.log("Event data:", event);
+        event.data().registrationSurvey.get().then((doc) => {
+          try {
+            if (!doc.exists)
+              return;
+
+            survey = doc;
+            console.log("survey data:", survey);
+            registrationQuestions = JSON.parse(survey.data().questionList);
+            buildForm(registrationQuestions.items, get('registration-questions'));
+            console.log(registrationQuestions);
+          } catch (e) {
+            console.log("Error parsing registration questions:", e);
+          }
+        });
+      }).catch((e) => {
+        console.log("Error getting document:", e);
+      });
+    } else {
+      console.log('not signed in');
+      query("#registration-text").innerHTML = "Hi there! To join the event, please log in to register!";
+      query("#registration-form").style.display = "none";
+      query("#login-button").style.display = "inline-block";
+    }
+  });
+
+  get('registration-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    try {
+      let user = firebase.auth().currentUser;
+      if (user) {
+        // TODO: Matching question answer
+        // update users collection
+        let surveyData = {
+          surveys: [{
+            survey: db.doc('surveys/' + survey.id),
+            response: '',
+          }],
+        }
+        let eventJoined = {
+          events: [{
+            event: db.doc('events/' + eventId),
+          }]
+        }
+        console.log("updating users collection");
+        db.collection('users').doc(user.uid).get().then((u) => {
+          if (!u.exists) {
+            db.collection('users').doc(user.uid).collection('private').doc('surveyData').set(surveyData);
+            db.collection('users').doc(user.uid).collection('private').doc('eventsJoined').set(eventJoined);
+          } else {
+            db.collection('users').doc(user.uid).collection('private').doc('surveyData').update(surveyData);
+            db.collection('users').doc(user.uid).collection('private').doc('eventsJoined').update(eventJoined);
+          }
+        });
+
+        // update events collection
+        console.log("updating events collection");
+        db.collection('events').doc(eventId).collection('usersJoined').doc(user.uid).set({});
+
+        // update surveys collection
+        console.log("updating surveys collection");
+        db.collection('surveys').doc(survey.id).collection('usersSubmitted').doc(user.uid).set({});
+      } else {
+        throw new Error('User is not signed in.');
+      }
+    } catch (e) {
+
+    }
+  });
+});
---
+++ signup.js
@@ -0,0 +1,167 @@
+// Convenient variables
+const get = document.getElementById.bind(document);
+const query = document.querySelector.bind(document);
+
+function buildForm(questions, formSelect) {
+    if (!questions || !formSelect) {
+        console.log('Cannot build form from quetions and formSelect');
+        return;
+    }
+
+    console.log('Building form from questions and formSelect');
+    formSelect.innerHTML = '';
+
+    for (let question of questions) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'dynamic-form-item');
+
+        var label = document.createElement('label');
+        label.setAttribute('class', 'dynamic-form-item-label');
+        label.setAttribute('for', question.id);
+        label.innerHTML = question.title;
+        if (question.isRequired) {
+            label.innerHTML += '<span class="dynamic-form-asterisk"> *</span>'
+        }
+
+        if (question.type === 'MULTIPLE_CHOICE' && question.choices) {
+            // question label
+            div.appendChild(label);
+
+            // radio button
+            if (question.hasOtherOption) {
+                question.choices.push('Other:');
+            }
+            for (let i = 0; i < question.choices.length; i++) {
+                let choice = question.choices[i];
+                var radio = document.createElement('input');
+                radio.setAttribute('class', 'dynamic-form-item-radio');
+                radio.setAttribute('type', 'radio');
+                radio.setAttribute('name', question.id);
+                radio.setAttribute('id', question.id + '.' + i);
+                radio.setAttribute('value', choice);
+
+                // handling isRequired
+                if (question.isRequired)
+                    radio.setAttribute('required', '');
+
+                // radio button label
+                var span = document.createElement('span');
+                span.setAttribute('for', question.id + '.' + i)
+                span.innerText = choice;
+
+                var label = document.createElement('label');
+                label.setAttribute('class', 'dynamic-form-choice-label');
+                label.appendChild(radio);
+                label.appendChild(span);
+
+                // handling 'other' option
+                if (choice === 'Other:') {
+                    var input = document.createElement('input');
+                    input.setAttribute('class', 'dynamic-form-choice-input');
+                    input.setAttribute('type', 'text');
+                    input.setAttribute('name', question.id);
+                    input.setAttribute('id', question.id + '.' + i + '.' + 'otherInput');
+                    input.addEventListener('click', (e) => {
+                        get(question.id + '.' + i).checked = true;
+                    });
+                    label.appendChild(input);
+                }
+
+                div.appendChild(label);
+            }
+        } else if (question.type === 'CHECKBOX' && question.choices) {
+            // question label
+            div.appendChild(label);
+
+            // checkbox
+            if (question.hasOtherOption) {
+                question.choices.push('Other:');
+            }
+            for (let i = 0; i < question.choices.length; i++) {
+                let choice = question.choices[i];
+                var checkbox = document.createElement('input');
+                checkbox.setAttribute('class', 'dynamic-form-item-checkbox');
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('name', question.id);
+                checkbox.setAttribute('id', question.id + '.' + i);
+                checkbox.setAttribute('value', choice);
+
+                // handling isRequired
+                if (question.isRequired) {
+                    if (i == 0)
+                        checkbox.setAttribute('required', '');
+                    checkbox.addEventListener('change', (e) => {
+                        var atLeastOneChecked = false;
+                        for (let j = 0; j < question.choices.length; j++) {
+                            atLeastOneChecked |= get(question.id + '.' + j).checked;
+                        }
+                        if (atLeastOneChecked) {
+                            get(question.id + '.' + 0).removeAttribute('required', '');
+                        } else {
+                            get(question.id + '.' + 0).setAttribute('required', '');
+                        }
+                    });
+                }
+
+                // checkbox label
+                var span = document.createElement('span');
+                span.setAttribute('for', question.id + '.' + i)
+                span.innerText = choice;
+
+                var label = document.createElement('label');
+                label.setAttribute('class', 'dynamic-form-choice-label');
+                label.appendChild(checkbox);
+                label.appendChild(span);
+
+                // handling 'other' option
+                if (choice === 'Other:') {
+                    var input = document.createElement('input');
+                    input.setAttribute('class', 'dynamic-form-choice-input')
+                    input.setAttribute('type', 'text');
+                    input.setAttribute('name', question.id);
+                    input.setAttribute('id', question.id + '.' + i + '.' + 'otherInput');
+                    input.addEventListener('click', (e) => {
+                        get(question.id + '.' + i).checked = true;
+                    });
+                    label.appendChild(input);
+                }
+
+                div.appendChild(label);
+            }
+        } else if (question.type === 'PARAGRAPH_TEXT' || question.type === 'TEXT') {
+            // question label
+            div.appendChild(label);
+
+            // text/area input
+            var input = document.createElement(question.type === 'TEXT' ? 'input' : 'textarea');
+            input.setAttribute('class', 'dynamic-form-text-input')
+            input.setAttribute('type', 'text');
+            input.setAttribute('id', question.id);
+            input.setAttribute('name', question.id);
+
+            // adjust text area height based on input
+            if (question.type === 'PARAGRAPH_TEXT') {
+                input.setAttribute('rows', 1);
+                input.addEventListener("input", (e) => {
+                    e.target.currentHeight = Math.max(e.target.scrollHeight, e.target.currentHeight ?? 0);
+                    e.target.style.height = e.target.currentHeight + "px";
+                });
+            }
+            if (question.isRequired)
+                input.setAttribute('required', '');
+            div.appendChild(input);
+        }
+
+        if (div.childElementCount) {
+            formSelect.appendChild(div);
+        }
+    }
+}
+
+function signOut() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+        // An error happened.
+    });
+}
